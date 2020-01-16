0b112e61bb200a6cc5cc96029abb53e23ccd2cde
nishida-boop <59200992+nishida-boop@users.noreply.github.com>
2020-01-16 17:40:52 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,4 @@
-$(function(){ 
- //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
- if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages, 7000);
-}
-  last_message_id = $('.message:last').data("message-id");
+$(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
@@ -56,29 +51,31 @@ $('#new_message').on('submit', function(e){
    processData: false,
    contentType: false
  })
-
   .done(function(data){
     var html = buildHTML(data);
+    $('.messages').append(html);
     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    $('.messages').append(html);      
     $('form')[0].reset();
-    $('form__submit').prop('disabled', false);
+    $('.form__submit').prop('disabled', false);
   })
-  .fail(function() {
+  .fail(function(){
     alert("メッセージ送信に失敗しました");
-});
-var reloadMessages = function() {
+  });
+  return false;
+ });
+ var reloadMessages = function() {
   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
   last_message_id = $('.message:last').data("message-id");
-  $.ajax({
-    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-    url: "api/messages",
-    //ルーティングで設定した通りhttpメソッドをgetに指定
-    type: 'get',
-    dataType: 'json',
-    //dataオプションでリクエストに値を含める
-    data: {id: last_message_id}
-  })
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
   .done(function(messages) {
     if (messages.length !== 0) {
     //追加するHTMLの入れ物を作る
@@ -88,69 +85,14 @@ var reloadMessages = function() {
       insertHTML += buildHTML(message)
     });
     //メッセージが入ったHTMLに、入れ物ごと追加
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $("#new_message")[0].reset();
-      $(".form__submit").prop("disabled", false);
-  }
- })
+    $('.messages').append(insertHTML);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    }
+  })
   .fail(function() {
-    alert('自動更新に失敗しました')
+    alart('error');
   });
-    var buildHTML = function(message) {
-      if (message.content && message.image) {
-        //data-idが反映されるようにしている
-        var html = `<div class="message" data-message-id=` + message.id + `>` +
-          `<div class="upper-message">` +
-            `<div class="upper-message__user-name">` +
-              message.user_name +
-            `</div>` +
-            `<div class="upper-message__date">` +
-              message.created_at +
-            `</div>` +
-          `</div>` +
-          `<div class="lower-message">` +
-            `<p class="lower-message__content">` +
-              message.content +
-            `</p>` +
-            `<img src="` + message.image + `" class="lower-message__image" >` +
-          `</div>` +
-        `</div>`
-        return html;
-      } else if (message.content) {
-        //同様に、data-idが反映されるようにしている
-        var html = `<div class="message" data-message-id=` + message.id + `>` +
-          `<div class="upper-message">` +
-            `<div class="upper-message__user-name">` +
-              message.user_name +
-            `</div>` +
-            `<div class="upper-message__date">` +
-              message.created_at +
-            `</div>` +
-          `</div>` +
-          `<div class="lower-message">` +
-            `<p class="lower-message__content">` +
-              message.content +
-            `</p>` +
-          `</div>` +
-        `</div>`
-      } else if (message.image) {
-        //同様に、data-idが反映されるようにしている
-        var html = `<div class="message" data-message-id=` + message.id + `>` +
-          `<div class="upper-message">` +
-            `<div class="upper-message__user-name">` +
-              message.user_name +
-            `</div>` +
-            `<div class="upper-message__date">` +
-              message.created_at +
-            `</div>` +
-          `</div>` +
-          `<div class="lower-message">` +
-            `<img src="` + message.image + `" class="lower-message__image" >` +
-          `</div>` +
-        `</div>`
-      };
-    };
-   };
- })
-})
+}
+};
+setInterval(reloadMessages, 7000);
+});
